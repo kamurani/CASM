4e5f691933e115a1f2afcd05fea5968a1d3739fd
Cam <cameronmcmenamie@gmail.com>
2022-07-02 17:11:10 +1000

    add parsing of config file on CLI

--- CASM/CLI.py
+++ CASM/CLI.py
@@ -1,11 +1,25 @@
 """Command line interface for CASM."""
 
+import pathlib
 import click
 
+from CASM.utils.config_parser import parse_config
+
 @click.command
-def main():
-    """Run app"""
-    click.echo("CASM")
+@click.option(
+    "-c",
+    "--config-path",
+    help="The path to the `.yml` config file",
+    type=click.Path(
+        exists=True, file_okay=True, dir_okay=False, path_type=pathlib.Path
+    ),
+)
+
+def main(config_path):
+    """Run job"""
+    config = parse_config(path=config_path) if config_path else None 
+    click.echo("Running...")
+    print(config)
 
 
 
