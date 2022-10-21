f5c7db61268e1d366bd1fb217fadcd3924418722
kamurani <cameronmcmenamie@gmail.com>
2022-10-21 18:10:31 +1100

    add notes

--- CASM/train.py
+++ CASM/train.py
@@ -16,8 +16,21 @@ TODO:
 
 SUPERVISED APPROACH: KINASE / SUBSTRATE PAIRS > GNN > CLASSIFIER 
 
+    i. substrate / kinase input: 0 or 1 output
+    ii.  substrate input: 1-hot output for all kinases (model must know structure implicitly somehow)
+
+        IDEA: 
+            what if we incorporate the input (substrate psite graph) and the kinase structure knowledge, by training
+            it with the kinase structure (the graph itself somehow (autoencoder?), embedding, etc.) AS THE EXPECTED
+            OUTPUT?
+
+            e.g. substrate / kinase training examples: 
+                input: substrate psite embeddings; output: substrate embedding
+                this way the neural network will 
 
 a. Generate kinase / substrate pairs (names / descriptors in array) with train / test split 
+    - take into account kinases with 1 sub (don't use this in test)
+    - randomly generate false pairs that are known to be non-interacting 
 
 b. Get actual graphs for feeding into network from (a.)
 
