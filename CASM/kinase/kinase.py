35166a139c35f7c6c10646772f0f2bd6a55962ff
kamurani <cameronmcmenamie@gmail.com>
2022-10-25 17:52:37 +1100

    add code for retrieving `(x,y,z)` coords for given kinase ATP site as predicted by AlphaFill

--- CASM/kinase/kinase.py
+++ CASM/kinase/kinase.py
@@ -2,13 +2,17 @@
 
 import os
 from pathlib import Path
-from typing import Callable, List, Union
+from typing import Callable, Dict, List, Union
 
 import click as c
 
 import json
 
 import requests
+
+
+# CIF file parsing
+from gemmi import cif
 
 
 class AlphaFillLoader():
@@ -24,6 +28,7 @@ class AlphaFillLoader():
         ignore: bool = False, # ignore missing directories 
 
         functions: List[Callable] = None, # TODO: specify subgraph selecting functions to be applied 
+        verbose: bool = True,
     ):
 
 
@@ -40,6 +45,8 @@ class AlphaFillLoader():
         self.config = config
         self.structure_extension = structure_extension
         self.metadata_extension = metadata_extension
+
+        self.verbose = verbose
 
     def _structure_url(self, acc_id: str):
         
@@ -78,6 +85,7 @@ class AlphaFillLoader():
         acc_id: str,
         overwrite: bool = True, # TODO # Overwrite already existing files with same name
     ):
+
         try:
             # Download metadata
             response = requests.get(self._metadata_url(acc_id))
@@ -93,7 +101,7 @@ class AlphaFillLoader():
         self,
         acc_id_list: List[str],
     ):
-
+        if self.verbose: print(f"Saving JSON and CIF files to {self.cif_dir}")
         for acc in acc_id_list:
             self.download_alphafill(acc)
 
@@ -101,14 +109,19 @@ class AlphaFillLoader():
     Parses AlphaFill metadata JSON file to find the best (lowest RMSD) scoring 
     transplant of a specified molecule ``label_comp_id``.
 
-    Uses ``local 
+    Uses local RMSD for backbone within 6Å of the transplanted molecule. 
+
+    Returns dictionary of 
+        analogue_id: 
+        asym_id: 
+        rmsd:
     """
     def get_asym_id_lowest_rmsd(
         self,
         acc_id: str, 
         molecule: str = "ATP",
 
-    ):
+    ) -> Dict[str, str]:
 
         # Check existence of path
         fn = self._metadata_path(acc_id)
@@ -118,24 +131,137 @@ class AlphaFillLoader():
         # Parse the JSON file 
         with open(fn) as f:
 
-            json.load(f)
+            data = json.load(f)
+
+            # OK, let's attack this JSON and find the lowest RMSD for our molecule
+
+            hits: list = data['hits']
 
 
+            # Get all hits for our molecule    
+            candidates = []
+            for hit in hits:
+
+                for t in hit['transplants']:
+
+                    if t['analogue_id'] == molecule:
+
+                        c = dict(
+                            analogue_id=t['analogue_id'],
+                            asym_id=t['asym_id'],
+                            rmsd=t['rmsd'],
+                        )
+                        candidates.append(c)
+
+            # Sort by RMSD, increasing order
+            c_list = sorted(candidates, key=lambda d: d['rmsd'])
+
+            # Return object with lowest RMSD
+            return c_list[0]
+
+
+    """
+    Gets ``asym_id`` of lowest scoring transplant molecule for a list of 
+    AlphaFill models 
+    """
+    def get_asym_id_list(
+        self,
+        acc_id_list: List[str], 
+        molecule: str = "ATP",
+    ) -> Dict[str, Dict]:
+
+        out = {}
+        for acc_id in acc_id_list:
+
+            try: 
+                data = self.get_asym_id_lowest_rmsd(acc_id=acc_id, molecule=molecule)
+
+            except:
+                # If no result found, set to empty dictionary
+                data = {}
+
+            out[acc_id] = data 
+
+        return out
 
     """
     Get coordinates of a particular atom for a file
+
+    e.g.
+
+    Gamma phosphorus for ATP (at kinase binding site)
     """
     def get_coordinates(
         self,
-        acc_id: str,
+        acc_id: Union[str, List[str]],
+        
+
+        rmsd_threshold: float = 6.0, # Ignore any data that has RMSD above this value
+
+        label_asym_id: str = None,      # This needs to be specified if there are multiple transplants of same molecule 
+                                        # By default this will use the metadata in same ``cif_dir`` directory from AlphaFill
+        type_symbol: str = "P",
+        label_atom_id: str = "PG",
+        label_comp_id: str = "ATP",
         
     ):
-        # TODO
-        path = self._get_structure_path(acc_id)
+        if type(acc_id) == str:
+            # Check existence of path
+            fn_cif = self._structure_path(acc_id)
+            if not os.path.isfile(fn_cif): raise FileNotFoundError(f"No file found: {fn_cif}")
 
-        # 
+            # Get ``asym_id`` of lowest scoring molecule (if not already specified to the function)
 
-        #molecule = 
+            if not label_asym_id:
+
+                # try / except here TODO
+
+                data: dict = self.get_asym_id_lowest_rmsd(acc_id=acc_id, molecule=label_comp_id)
+                label_asym_id = data['asym_id']
+
+
+            # Parse CIF file
+            doc = cif.read_file(fn_cif)
+            block = doc.sole_block() 
+
+            table = block.find(
+                "_atom_site.", 
+                ["type_symbol", "label_atom_id", "label_comp_id", "label_asym_id",  # Used in the query checking (input)
+                "Cartn_x", "Cartn_y", "Cartn_z"]                                    # cartesian coordinates      (output)
+            )
+            tags = table.tags 
+
+            query = [type_symbol, label_atom_id, label_comp_id, label_asym_id]
+            # TODO more elegant way of checking row match?
+
+            # iterate through rows
+            output = None
+            for row in table:
+                
+                if (
+                    row["_atom_site.type_symbol"]   == type_symbol      and
+                    row["_atom_site.label_atom_id"] == label_atom_id    and
+                    row["_atom_site.label_comp_id"] == label_comp_id    and
+                    row["_atom_site.label_asym_id"] == label_asym_id
+                ):
+
+                    output = (
+                        row["_atom_site.Cartn_x"],
+                        row["_atom_site.Cartn_y"],
+                        row["_atom_site.Cartn_z"],
+                    )
+
+            # no match
+            if not output: raise Exception(f"No element found for query: {query}")
+
+            return output
+
+
+        for e in block.find_loop("_atom_site.label_atom_id"):
+            print(e)
+
+        
+
         (x,y,z) = (0,0,0)
         print(f"{acc_id}\t{x}\t{y}\t{z}")
         
@@ -181,16 +307,14 @@ def main(
     alphafill_dir,
 ):
 
-    print(f"Saving JSON and CIF files to {alphafill_dir}")
+    
     acc_ids = load_from_file()
 
     #acc_ids = ["Q9UHD2", "Q9WTU6"]
 
+    acc_ids = ["Q96PY6"]
+
     # Alphafill
-
-
-
-
     loader = AlphaFillLoader(
         cif_dir=alphafill_dir,
     )
@@ -198,9 +322,35 @@ def main(
     # Download (if required)
     #loader.download_alphafill_list(acc_ids)
 
-    loader.get_coordinates()
+
+    for acc_id in acc_ids:
+
+        
+        (x, y, z) = loader.get_coordinates(
+            acc_id=acc_id,
+        )
+
+        l = [acc_id, x, y, z]
+
+        try:
+            pass  
+        except:
+
+            l = [acc_id, 'UNKNOWN']
+
+        print('\t'.join([str(x) for x in l]))
+
+    return
 
 
+    ATP: dict = loader.get_asym_id_list(acc_ids)
+
+    # print
+    for k in ATP.keys():
+        try:
+            print(f"{k}\t{ATP[k]['asym_id']}\t{ATP[k]['rmsd']}")
+        except:
+            print(k, "NO DATA")
 
 if __name__ == "__main__":
     main()
