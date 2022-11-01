d54c8daf3c1a3f5da8aa756b705a7a8b1bab7b37
kamurani <cameronmcmenamie@gmail.com>
2022-11-01 17:20:20 +1100

    fix import to include torch_geometric version of `DataLoader` class

--- CASM/train.py
+++ CASM/train.py
@@ -101,13 +101,30 @@ protein id known,
 """
 
 
-
+import pandas as pd
 import torch
-from torch.utils.data import DataLoader, random_split
+from torch.utils.data import random_split
+from torch_geometric.data import DataLoader
 
 import math
 
 from CASM.load_training_data import dataset
+from CASM.model import GCNN, AttGNN
+
+import torch_optimizer as optim 
+from torch.optim.lr_scheduler import MultiStepLR, ReduceLROnPlateau
+from metrics import *
+
+
+import torch.nn as nn
+import networkx as nx
+import torch.nn.functional as F
+from torch_geometric.nn import MessagePassing
+from torch_geometric.utils import add_self_loops, degree
+
+
+device = torch.device("cuda:1") if torch.cuda.is_available() else torch.device("cpu")
+
 
 size = len(dataset)
 train_ratio = 0.8
@@ -129,7 +146,72 @@ test_dataloader = DataLoader(test_data, batch_size=batch_size, num_workers=num_w
 
 def train(model, device, train_dataloader, optimizer, epoch):
 
-    pass
+    print(f"Training on {len(train_dataloader)} samples....")
+    model.train()
+    loss_func = nn.MSELoss()
+
+    predictions_tr = torch.Tensor()
+    scheduler = MultiStepLR(optimizer, milestones=[1,5], gamma=0.5)
+    labels_tr = torch.Tensor()
+
+    for count, (kinase, site, mod_rsd, label) in enumerate(train_dataloader):
+
+        kinase = kinase.to(device)
+        site = site.to(device)
+
+        optimizer.zero_grad()
+        output = model(kinase, site)
+        predictions_tr = torch.cat((predictions_tr, output.cpu()))
+
+        labels_tr = torch.cat((labels_tr, label.view(-1,1).cpu()), 0)
+        loss = loss_func(output, label.view(-1,1).float().to(device))
+        loss.backward()
+        optimizer.step()
+
+    scheduler.step()
+    labels_tr = labels_tr.detach().numpy()
+    predictions_tr = predictions_tr.detach().numpy()
+    acc_tr = get_accuracy(labels_tr, predictions_tr , 0.5)
+    print(f'Epoch {epoch-1} / 30 [==============================] - train_loss : {loss} - train_accuracy : {acc_tr}')
+
+
+model = GCNN()
+model.to(device)
+num_epochs = 100
+best_accuracy = 0
+lr = 0.001
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+
+for epoch in range(num_epochs):
+    
+    train(model, device, train_dataloader, optimizer, epoch+1)
+    G, P = predict(model, device, testloader)
+
+    loss = get_mse(G,P)
+    accuracy = get_accuracy(G,P, 0.5)
+    print(f'Epoch {epoch}/ {num_epochs} [==============================] - val_loss : {loss} - val_accuracy : {accuracy}')
+
+
+    if(accuracy > best_accuracy):
+        best_accuracy = accuracy
+        best_acc_epoch = epoch
+        torch.save(model.state_dict(), "../GCN.pth") #path to save the model
+        print("Model")
+    if(loss< min_loss):
+        epochs_no_improve = 0
+        min_loss = loss
+        min_loss_epoch = epoch
+    elif loss > min_loss:
+        epochs_no_improve += 1
+    if epoch > 5 and epochs_no_improve == n_epochs_stop:
+        print('Early stopping!' )
+        early_stop = True
+        break
+
+print(f'min_val_loss : {min_loss} for epoch {min_loss_epoch} ............... best_val_accuracy : {best_accuracy} for epoch {best_acc_epoch}')
+print("Model saved")
+
+
 
 
 
@@ -142,3 +224,6 @@ compare to ground truth: see if any new ones get added? (ground truth includes "
 
 see if ground truth is a subset of predictions? (should be, if model has learnt training)
 """
+
+
+
