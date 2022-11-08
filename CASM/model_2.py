14d90051eb2eefc29474ebf8ea176ad9dd723f2a
kamurani <cameronmcmenamie@gmail.com>
2022-11-08 12:56:39 +1100

    add GCN model for double (2-layer) convolutions

--- CASM/model_2.py
+++ CASM/model_2.py
@@ -138,7 +138,157 @@ class GCNN2(nn.Module):
         else: 
             raise NotImplementedError(f"Embedding method '{self.embedding_method}' not implemented.")
         
-    
+
+        x = self.pro_fc1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+
+        x = self.fc1(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        x = self.fc2(x)
+        x = self.relu(x)
+        x = self.dropout(x)
+        out = self.out(x)
+
+        if self.output_activation in ["softmax"]:
+            out = self.softmax(out)
+        else: 
+            raise NotImplementedError(f"Output activation '{self.output_activation}' not implemented.")
+
+	
+        return out
+
+class GCNN2_Double(nn.Module):
+    def __init__(
+        self, 
+        n_output: int = len(KINASE_FAMILY_DICT), 
+        num_features_pro: int = None, # Number of features is 1280 if using ESM per-residue embeddings.
+        
+        output_dim: int = 128, # Length after the first FC layer 
+        dropout: float = 0.2,
+
+        features: list = [],
+
+        embedding_method: str = "node",         # or "gep" i.e. global pooling
+        use_residue_encoding: str = "meiler", #"1-hot",    # how to encode the residues (NOTE: assumes these features have already been created in the sample data)
+        output_activation: str = "softmax",     # TODO: switch between this and individual sigmoid
+    ):
+
+        if num_features_pro is None:
+            em_len = 20 if embedding_method == "1-hot" else 7 
+            num_features_pro = 20 + len(features)
+
+        self.features = features
+
+        super(GCNN2_Double, self).__init__()
+
+        print('GCNN Loaded')
+        print(f"Using {use_residue_encoding} node embeddings ...")
+
+        self.embedding_method = embedding_method.lower()
+        self.use_residue_encoding = use_residue_encoding.lower()
+        self.output_activation = output_activation.lower()
+        # for protein 1
+        self.n_output = n_output
+        self.pro_conv = GCNConv(num_features_pro, num_features_pro)
+
+        self.pro_conv2 = GCNConv(num_features_pro, num_features_pro) # 2nd GCN layer
+
+        # Layer 2 
+        # TODO
+
+        self.pro_fc1 = nn.Linear(num_features_pro, output_dim)
+
+        self.relu = nn.LeakyReLU()
+        self.dropout = nn.Dropout(dropout)
+
+        self.sigmoid = nn.Sigmoid()
+        self.softmax = nn.Softmax(dim=1)
+
+        self.fc1 = nn.Linear(output_dim, 128)
+        self.fc2 = nn.Linear(128, 64)
+        self.out = nn.Linear(64, self.n_output)
+
+    def forward(self, phosphosite: Union[torch_geometric.data.Data, torch_geometric.data.Batch]):
+
+        
+        node_index  = phosphosite.node_index
+        num_nodes   = phosphosite.num_nodes
+        psite_batch = phosphosite.batch
+        edge_index  = phosphosite.edge_index
+        
+        # Get node features as required 
+        
+        if self.use_residue_encoding in ["one-hot", "1-hot"]:
+            residue_encoding = phosphosite.amino_acid_one_hot
+        elif self.use_residue_encoding in ["meiler", "meiler-embedding"]:
+            residue_encoding = phosphosite.meiler 
+        elif self.use_residue_encoding in ["esm", "esm-embedding"]:
+            pass
+            residue_encoding = phosphosite.esm_embedding # TODO: check this 
+
+        else:
+            raise NotImplementedError(f"Residue encoding '{self.use_residue_encoding}' not implemented.")
+
+        # normalise 
+        #print(phosphosite.asa)
+        #asa = F.normalize(torch.tensor(phosphosite.asa[0]).float(), p=2, dim=0)
+
+        #print(asa)
+        
+        #b_factor = [[b / 100 for b in phosphosite.b_factor[0]]] # Get pLDDT as percentage
+
+        #b_factor = phosphosite.b_factor
+        #asa = phosphosite.asa
+
+
+        #self.features 
+
+        #features = [eval(f"phosphosite.{f}") for f in self.features]
+        features = []
+        if "asa" in self.features:
+            features.append(phosphosite.asa)
+        if "b_factor" in self.features:
+            features.append(phosphosite.b_factor)
+        
+        features.append(residue_encoding)
+        #features = [residue_encoding, asa, b_factor]   # , pro2_data.asa, pro2_data.phi, pro2_data.psi]
+        
+        # Concatenate along 1st dim (dim=0) to get all nodes for all batches in one tensor
+        for i, f in enumerate(features):
+                
+            features[i] = torch.as_tensor(np.concatenate([np.array(i, dtype=np.float32) for i in f]))
+            f = features[i]
+            # Vertically stack if only one feature long
+            if f.shape == torch.Size([num_nodes]):
+                features[i] = f.view(-1, 1)
+
+        # Concatenate all features together
+        x = torch.cat(features, dim=1)
+
+
+        x = x.float()
+
+        
+
+        # First graph convolution layer 
+        x = self.pro_conv(x, edge_index)
+        x = self.relu(x) 
+
+        # Second graph convolution layer
+        x = self.pro_conv2(x, edge_index)
+        x = self.relu()
+
+        # Pooling
+        if self.embedding_method in ["gep", "global"]:
+            x = gep(x, psite_batch)
+        elif self.embedding_method in ["node", "site"]:
+            x = x[node_index]
+        else: 
+            raise NotImplementedError(f"Embedding method '{self.embedding_method}' not implemented.")
+        
+        
         x = self.pro_fc1(x)
         x = self.relu(x)
         x = self.dropout(x)
